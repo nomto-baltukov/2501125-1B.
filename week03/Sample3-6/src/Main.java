00cfa2d0df99e8b369707c9277ed1ea2c6b0fa80
nomto-baltukov <namutobaozi@gmail.com>
2025-09-17 11:16:53 +0900

    3 추가

---
+++ week03/Sample3-6/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int time = 5800;
+        int work = 5;
+        int day = 7;
+        int money = 25000;
+
+        System.out.println("나의 시급 : %d %c \n", time, '\uffe6');
+        System.out.printf("하루에 일한 시간 : %d 시간 \n", work);
+        System.out.printf("며칠 일했니요: %d 일 \n", day);
+        System.out.printf("나의 지갑 돈 : %, d %c \n", money, '\uffe6');
+    }
+}
