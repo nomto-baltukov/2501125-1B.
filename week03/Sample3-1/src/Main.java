2000075016beeb23f744a4722605336a0583435a
nomto-baltukov <namutobaozi@gmail.com>
2025-09-17 10:12:32 +0900

    3 추가

---
+++ week03/Sample3-1/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;       // 선언문   a를  변수
+        float b = 0.0f;
+        long c = 0L;
+        byte d = 0;
+        short f = 0;
+        boolean t = false;
+        char g = ' ';
+
+        final float PI = 3.141592f;
+        PI = 3.14f;
+
+        String y = "";
+        String z = new String("");
+
+        a = 5;       // 대업문   5를 정수형 리터럴
+        b = 5.5f;    //
+        c = 5;
+
+        a = a + 5;
+        d = -128;
+    }
+}
