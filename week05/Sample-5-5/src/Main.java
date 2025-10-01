8a566ce96d0edfc9fa53df125247548cea91eef0
nomto-baltukov <namutobaozi@gmail.com>
2025-10-01 10:56:53 +0900

    5 주차

---
+++ week05/Sample-5-5/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int data = 0;
+        String result = "";
+
+        System.out.print("양의 정수 입력");
+        data = keyboard.nextInt();
+
+        result = data % 2 == 0 ? "짝수" : "홀수";
+
+    }
+}
