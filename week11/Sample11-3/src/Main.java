a18f0b2250da2b6a36657d2c4bf2d676312df103
nomto-baltukov <namutobaozi@gmail.com>
2025-11-19 10:30:34 +0900

    10 주차

---
+++ week11/Sample11-3/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[][] sales={ {2010, 11230, 12345, 21495, 21543, 0},
+                        {2010, 11230, 12345, 21495, 21543, 0},
+                        {2010, 11230, 12345, 21495, 21543, 0}};
+
+        float[] avg = new float[sales.length];
+
+        System.out.println("*********************************");
+        System.out.println("년도 1분기 2분기 3분기 4분기ㅣ");
+    }
+}
