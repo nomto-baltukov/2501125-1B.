bb293a8803e00d5444bd4aa8e65ab47fec43a2cd
nomto-baltukov <namutobaozi@gmail.com>
2025-11-19 09:29:24 +0900

    10 주차

---
+++ week11/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       int[][] test = new int[4][];
+       test[0] = new int[3];
+       test[1] = new int[2];
+       test[2] = new int[3];
+       test[3] = new int[2];
+
+       int[][] temp = new int[][] {{1,2,3}, {2,3},{5,6,7}};
+    }
+}
