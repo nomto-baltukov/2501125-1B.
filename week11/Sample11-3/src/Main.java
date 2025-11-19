d3f3d7d153f0a01d2609ac8ae75db3f38e34d42f
nomto-baltukov <namutobaozi@gmail.com>
2025-11-19 10:45:29 +0900

    10 주차

--- week11/Sample11-3/src/Main.java
+++ week11/Sample11-3/src/Main.java
@@ -1,14 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[][] sales={ {2010, 11230, 12345, 21495, 21543, 0},
-                        {2010, 11230, 12345, 21495, 21543, 0},
-                        {2010, 11230, 12345, 21495, 21543, 0}};
+        int[][] sales = {
+                {2010, 11230, 12345, 21495, 21543, 0},
+                {2011, 14500, 12300, 16700, 18900, 0},
+                {2012, 13400, 15800, 12000, 11000, 0}
+        };
 
         float[] avg = new float[sales.length];
 
-        System.out.println("*********************************");
-        System.out.println("년도 1분기 2분기 3분기 4분기ㅣ");
+        // 합계 계산
+        for (int i = 0; i < sales.length; i++) {
+            for (int j = 1; j < sales[i].length - 1; j++) { // j++
+                sales[i][sales[i].length - 1] += sales[i][j];
+            }
+            // 평균 계산
+            avg[i] = sales[i][sales[i].length - 1] / 4.0f;
+        }
+
+        // 테이블 출력
+        System.out.println("***************************************");
+        System.out.println("년도\t1분기\t2분기\t3분기\t4분기\t합계\t평균");
+        System.out.println("***************************************");
+
+        for (int i = 0; i < sales.length; i++) {
+            System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\t%.2f\n",
+                    sales[i][0], sales[i][1], sales[i][2], sales[i][3], sales[i][4],
+                    sales[i][5], avg[i]);
+        }
+
     }
 }
